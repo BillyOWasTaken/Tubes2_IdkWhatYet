e6018863eff993750749807581842d0edbf6cb00
Jennifer Khang <jenniferkhang07@gmail.com>
2026-04-14 20:31:46 +0700

    feat: webscrapper using axios

--- src/backend/src/data/WebScrapper.ts
+++ src/backend/src/data/WebScrapper.ts
@@ -1,4 +1,19 @@
 import axios from "axios";
-async function = scarpeSite(){
-    const response = awat axios.get()
+import { ScraperError } from "../core/ErrorHandling";
+
+export class WebScraper {
+    async scrap(url: string): Promise<string> {
+        try {
+            const response = await axios.get(url, { timeout: 5000 });
+            return response.data;
+        } catch (error:any) {
+            const status = error.response?.status;
+
+            if (status == 404) {
+                throw new ScraperError("[Scrapper] Page tidak ditemukan", 404);
+            }
+
+            throw new ScraperError("[Scrapper] Gagal mengambil HTML", status);
+        }
+    }
 }
